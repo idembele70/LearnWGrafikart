2f29b6dbc13b0af4153591ce6915a1eba4b3273d
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-07-11 22:51:17 +0200

    [Paramters] Done

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,8 @@
 // type challenges
-type tesla = ["tesla", "model 3", "model X", "model Y"];
-type spaceX = [
-  "FALCON 9",
-  "FALCON HEAVY",
-  "DRAGON",
-  "STARSHIP",
-  "HUMAN SPACEFLIGHT"
-];
-type Length<T extends readonly any[]> = T["length"];
-type teslaLength = Length<tesla>; // expected 4
-type spaceXLength = Length<spaceX>; // expected 5
+const foo = (arg1: string, arg2: number): void => {};
+type MyParameters<T extends Function> = T extends (
+  ...args: infer ArgT
+) => unknown
+  ? ArgT
+  : [];
+type FunctionParamsType = MyParameters<typeof foo>; // [arg1: string, arg2: number]
