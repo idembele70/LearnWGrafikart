c0081c9c03c88cfb13a00cecdfd870fceda70c86
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-08-01 17:18:43 +0200

    [Typechalenge] pop from array

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,18 @@
 // type challenges
+type Equal<T, U> = <V>() => (V extends T ? 1 : 2) extends <V>() => V extends U
+  ? 1
+  : 2
+  ? true
+  : false;
+type Expect<T extends true> = T;
 
-type MyParameters<T> = T extends (...args: infer R) => any ? R : never;
+// Exercice
+type arr1 = ["a", "b", "c", "d"];
+type arr2 = [3, 2, 1];
 
-const foo = (arg1: string, arg2: number): void => {};
+type Pop<T extends any[]> = T extends [...infer TPop, PropertyKey]
+  ? TPop
+  : never;
 
-type FunctionParamsType = MyParameters<typeof foo>; // [arg1: string, arg2: number]
+type re1 = Pop<arr1>; // expected to be ['a', 'b', 'c']
+type re2 = Pop<arr2>; // expected to be [3, 2]
