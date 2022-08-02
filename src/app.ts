741dd47c08f41e7b0b732d96f41133f68a71c67a
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-08-03 01:16:18 +0200

    [Type Challenge] Last of array

--- src/app.ts
+++ src/app.ts
@@ -7,27 +7,13 @@ type Equal<T, U> = <V>() => (V extends T ? 1 : 2) extends <V>() => V extends U
 type Expect<T extends true> = T;
 
 // Exercice
-interface Todo {
-  title: string;
-  description: string;
-  completed: boolean;
-}
-type MyExclude<T, U> = T extends U ? never : T;
-type MyOmit<T, U extends keyof T> = {
-  [Key in MyExclude<keyof T, U>]: T[Key];
-};
-type MyPick<T, U extends keyof T> = {
-  [Key in keyof T as Key extends U ? Key : never]: T[Key];
-};
-type MyReadonly2<T, K extends keyof T = keyof T> = Readonly<MyPick<T, K>> &
-  MyOmit<T, K>;
 
-const todo: MyReadonly2<Todo, "title" | "description"> = {
-  title: "Hey",
-  description: "foobar",
-  completed: false,
-};
+type arr1 = ["a", "b", "c"];
+type arr2 = [3, 2, 1];
 
-todo.title = "Hello"; // Error: cannot reassign a readonly property
-todo.description = "barFoo"; // Error: cannot reassign a readonly property
-todo.completed = true; // OK
+type DontUseAny = PropertyKey | object;
+
+type Last<T extends DontUseAny[]> = [DontUseAny, ...T][T["length"]];
+
+type tail1 = Last<arr1>; // expected to be 'c'
+type tail2 = Last<arr2>; // expected to be 1
