b48daf95787c0b94a789e8d208803793aa6475c8
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-08-19 02:43:29 +0200

    [Type challenge] Chainable

--- src/app.ts
+++ src/app.ts
@@ -7,41 +7,33 @@ type Equal<T, U> = <V>() => (V extends T ? 1 : 2) extends <V>() => V extends U
 type Expect<T extends true> = T;
 
 // Exercice
+type Chainable<T = {}> = {
+  option<K extends string, V>(
+    key: K extends keyof T ? (V extends T[K] ? never : K) : K,
+    value: V
+  ): Chainable<{
+    [Key in K | keyof T]: Key extends K
+      ? V
+      : Key extends keyof T
+      ? T[Key]
+      : never;
+  }>;
+  get: () => T;
+};
 
-type NodeA = {
-  type: "A";
+declare const config: Chainable;
+
+const result = config
+  .option("foo", 123)
+  .option("name", "type-challenges")
+  .option("bar", { value: "Hello World" })
+  .get();
+
+// expect the type of result to be:
+interface Result {
+  foo: number;
   name: string;
-  flag: number;
-};
-
-type NodeB = {
-  type: "B";
-  id: number;
-  flag: number;
-};
-
-type NodeC = {
-  type: "C";
-  name: string;
-  flag: number;
-};
-
-type Nodes = NodeA | NodeB | NodeC;
-type ReplaceKeys<T, U, O> = T extends Record<string, unknown>
-  ? {
-      [Key in keyof T]: Key extends keyof O
-        ? O[Key]
-        : Key extends U
-        ? never
-        : T[Key];
-    }
-  : never;
-
-type ReplacedNodes = ReplaceKeys<
-  Nodes,
-  "name" | "flag",
-  { name: number; flag: string }
->; // {type: 'A', name: number, flag: string} | {type: 'B', id: number, flag: string} | {type: 'C', name: number, flag: string}
-// would replace name from string to number, replace flag from number to string.
-
-type ReplacedNotExistKeys = ReplaceKeys<Nodes, "name", { aa: number }>; // {type: 'A', name: never, flag: number} | NodeB | {type: 'C', name: never, flag: number} // would replace name to never
+  bar: {
+    value: string;
+  };
+}
