4688a97c6ceb97776d92ac580334be50ae00ebd4
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-08-23 01:43:21 +0200

    [Type Challenge] Trim

--- src/app.ts
+++ src/app.ts
@@ -7,33 +7,9 @@ type Equal<T, U> = <V>() => (V extends T ? 1 : 2) extends <V>() => V extends U
 type Expect<T extends true> = T;
 
 // Exercice
-type Chainable<T = {}> = {
-  option<K extends string, V>(
-    key: K extends keyof T ? (V extends T[K] ? never : K) : K,
-    value: V
-  ): Chainable<{
-    [Key in K | keyof T]: Key extends K
-      ? V
-      : Key extends keyof T
-      ? T[Key]
-      : never;
-  }>;
-  get: () => T;
-};
 
-declare const config: Chainable;
+type Trim<S extends string> = S extends `${" "}${infer Middle}${" "}`
+  ? Trim<Middle>
+  : S;
 
-const result = config
-  .option("foo", 123)
-  .option("name", "type-challenges")
-  .option("bar", { value: "Hello World" })
-  .get();
-
-// expect the type of result to be:
-interface Result {
-  foo: number;
-  name: string;
-  bar: {
-    value: string;
-  };
-}
+type trimmed = Trim<"  Hello World  ">; // expected to be 'Hello World'
