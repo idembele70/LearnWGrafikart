32dc13b992b58e0bd6a1580552fe4a6c8cda5753
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-09-08 02:27:37 +0200

    [Type challenge] requiredByKeys

--- src/app.ts
+++ src/app.ts
@@ -8,25 +8,24 @@ type Expect<T extends true> = T;
 
 // Exercice
 
-type PString1 = "";
-type PString2 = "+85%";
-type PString3 = "-85%";
-type PString4 = "85%";
-type PString5 = "85";
+interface User {
+  name?: string;
+  age?: number;
+  address?: string;
+}
 
-type PercentageParser<S extends string> =
-  S extends `${infer First}${infer Rest}`
-    ? First extends "+" | "-"
-      ? Rest extends `${infer R}%`
-        ? [First, R, "%"]
-        : [First, Rest, ""]
-      : S extends `${infer R}%`
-      ? ["", R, "%"]
-      : ["", S, ""]
-    : ["", "", ""];
+type Merge<O> = {
+  [Key in keyof O]: O[Key];
+};
+type MyExclude<T, U> = T extends U ? never : T;
+type RequiredByKeys<O, K> = Merge<
+  {
+    [Key in K as Key extends keyof O ? Key : never]: Key extends keyof O
+      ? MyExclude<O[Key], undefined>
+      : never;
+  } & {
+    [Key in Exclude<keyof O, K>]?: O[Key];
+  }
+>;
 
-type R1 = PercentageParser<PString1>; // expected ['', '', '']
-type R2 = PercentageParser<PString2>; // expected ["+", "85", "%"]
-type R3 = PercentageParser<PString3>; // expected ["-", "85", "%"]
-type R4 = PercentageParser<PString4>; // expected ["", "85", "%"]
-type R5 = PercentageParser<PString5>; // expected ["", "85", ""]
+type UserRequiredName = RequiredByKeys<User, "name">; // { name: string; age?: number; address?: string }
