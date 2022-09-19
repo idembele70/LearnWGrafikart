851d0f2b879df33da9a12cbebfab78e12096121f
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-09-20 01:32:34 +0200

    [Type challenge] readonly

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,19 @@ type Equal<T, U> = <V>() => (V extends T ? 1 : 2) extends <V>() => V extends U
 type Expect<T extends true> = T;
 
 // Exercice
+interface Todo {
+  title: string;
+  description: string;
+}
 
-type IsUnion<T, C = T> = T extends C ? ([C] extends [T] ? false : true) : false;
+type MyReadonly<T> = {
+  readonly [Key in keyof T]: T[Key];
+};
 
-type case1 = IsUnion<string>; // false
-type case2 = IsUnion<string | number>; // true
-type case3 = IsUnion<[string | number]>; // false
+const todo: MyReadonly<Todo> = {
+  title: "Hey",
+  description: "foobar",
+};
+
+todo.title = "Hello"; // Error: cannot reassign a readonly property
+todo.description = "barFoo"; // Error: cannot reassign a readonly property
