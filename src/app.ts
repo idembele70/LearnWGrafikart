ecde272cf246e838f7d04d60eb94adfa8615f17c
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-09-29 14:45:25 +0200

    [Type Challenge] IsUnion

--- src/app.ts
+++ src/app.ts
@@ -7,20 +7,9 @@ type Equal<T, U> = <V>() => (V extends T ? 1 : 2) extends <V>() => V extends U
 type Expect<T extends true> = T;
 
 // Exercice
-interface User {
-  name?: string;
-  age?: number;
-  address?: string;
-}
-type Merge<O> = {
-  [Key in keyof O]: O[Key];
-};
-type RequiredByKeys<O, K> = Merge<
-  {
-    [Key in keyof O as Key extends K ? Key : never]-?: O[Key];
-  } & {
-    [Key in keyof O as Key extends K ? never : Key]: O[Key];
-  }
->;
 
-type UserRequiredName = RequiredByKeys<User, "name">; // { name: string; age?: number; address?: string }
+type IsUnion<T, C = T> = T extends C ? ([C] extends [T] ? false : true) : false;
+
+type case1 = IsUnion<string>; // false
+type case2 = IsUnion<string | number>; // true
+type case3 = IsUnion<[string | number]>; // false
