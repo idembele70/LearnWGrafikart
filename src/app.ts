e527d2e72216be9b3df550f740aeeee295cc0e04
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-10-18 16:32:26 +0200

    [Type Challenge] Permutation

--- src/app.ts
+++ src/app.ts
@@ -8,50 +8,10 @@ type Expect<T extends true> = T;
 
 // Exercice
 
-type NumberToTuple<T extends number, A extends 1[] = []> = A["length"] extends T
-  ? A
-  : NumberToTuple<T, [...A, 1]>;
-
-type Plus<T extends number, U extends number> = [
-  ...NumberToTuple<T>,
-  ...NumberToTuple<U>
-]["length"] &
-  number;
-
-type MinusOne<T extends number> = NumberToTuple<T> extends [any, ...infer Rest]
-  ? Rest["length"]
-  : 0;
-
-type GreatherOrEqual<T extends number, U extends number> = T extends U
-  ? true
-  : T extends 0
-  ? false
-  : U extends 0
-  ? true
-  : GreatherOrEqual<MinusOne<T>, MinusOne<U>>;
-
-type Greater<T extends number, U extends number> = T extends 0
-  ? false
-  : U extends 0
-  ? true
-  : Greater<MinusOne<T>, MinusOne<U>>;
-
-type A = GreatherOrEqual<1, 1>;
-
-type Fill<
-  T extends any[],
-  N,
-  Start extends number = 0,
-  End extends number = T["length"],
-  Idx extends number = 0
-> = GreatherOrEqual<Start, End> extends true
-  ? T
-  : T extends [infer First, ...infer Rest]
-  ? GreatherOrEqual<Idx, Start> extends true
-    ? Greater<End, Idx> extends true
-      ? [N, ...Fill<Rest, N, Start, End, Plus<Idx, 1>>]
-      : [First, ...Fill<Rest, N, Start, End, Plus<Idx, 1>>]
-    : [First, ...Fill<Rest, N, Start, End, Plus<Idx, 1>>]
+type Permutation<T, C = T> = [T] extends [never]
+  ? []
+  : C extends C
+  ? [C, ...Permutation<Exclude<T, C>>]
   : [];
 
-type exp = Fill<[1, 2, 3], true, 1, 3>; // expected to be [1, true, true]
+type perm = Permutation<"A" | "B" | "C">; // ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']
