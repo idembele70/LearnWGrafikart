8274346a1ccc7b7081765561617762b6a621b7fa
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-11-27 00:55:50 +0100

    [Type Challenge] Last Of Array

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,10 @@ type Expect<T extends true> = T;
 
 // Exercice
 
-const fn = (v: boolean) => {
-  if (v) return 1;
-  else return 2;
-};
+type arr1 = ["a", "b", "c"];
+type arr2 = [3, 2, 1];
 
-type MyReturnType<F extends Function> = F extends (arg: any) => infer RT
-  ? RT
-  : never;
+type Last<A extends any[]> = A extends [...any, infer R] ? R : never;
 
-type a = MyReturnType<typeof fn>; // should be "1 | 2"
+type tail1 = Last<arr1>; // expected to be 'c'
+type tail2 = Last<arr2>; // expected to be 1
