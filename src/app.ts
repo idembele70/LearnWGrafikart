babd835a0e5e7b56f0b9e9ecf699fef730380653
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-12-01 01:45:23 +0100

    [Type Challenge] Permutation

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,10 @@ type Expect<T extends true> = T;
 
 // Exercice
 
-type Fn = (a: number, b: string) => number;
+type Permutation<U, C = U> = [U] extends [never]
+  ? []
+  : U extends any
+  ? [U, ...Permutation<Exclude<C, U>>]
+  : [];
 
-type Result = AppendArgument<Fn, boolean>;
-// expected be (a: number, b: string, x: boolean) => number
-
-type AppendArgument<F extends Function, A> = F extends (
-  ...args: infer Args
-) => infer RT
-  ? (...args: [...Args, A]) => RT
-  : never;
+type perm = Permutation<"A" | "B" | "C">; // ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']
