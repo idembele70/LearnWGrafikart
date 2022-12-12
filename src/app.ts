4e1b802a963a73cb5c1b9fef13c5f93e62b3a0a7
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-12-12 01:53:47 +0100

    [Type Challenge] IsUnion Done !

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,13 @@
 // type challenges
-type Equal<T, U> = <V>() => (V extends T ? 1 : 2) extends <V>() => V extends U
-  ? 1
-  : 2
-  ? true
+
+type IsUnion<T, C = T> = [T] extends [never]
+  ? false
+  : T extends C
+  ? [C] extends [T]
+    ? false
+    : true
   : false;
-type Expect<T extends true> = T;
 
-//
-
-interface UppercaseChars {
-  A: "a";
-  B: "b";
-  C: "c";
-  D: "d";
-  E: "e";
-  F: "f";
-  G: "g";
-  H: "h";
-  I: "i";
-  J: "j";
-  K: "k";
-  L: "l";
-  M: "m";
-  N: "n";
-  O: "o";
-  P: "p";
-  Q: "q";
-  R: "r";
-  S: "s";
-  T: "t";
-  U: "u";
-  V: "v";
-  W: "w";
-  X: "x";
-  Y: "y";
-  Z: "z";
-}
-
-type KebabCase<
-  S extends string,
-  isFirst = true
-> = S extends `${infer First}${infer Rest}`
-  ? First extends keyof UppercaseChars
-    ? `${isFirst extends true ? "" : "-"}${UppercaseChars[First]}${KebabCase<
-        Rest,
-        false
-      >}`
-    : `${First}${KebabCase<Rest, false>}`
-  : S;
-
-type FooBarBaz = KebabCase<"FooBarBaz">;
-const foobarbaz: FooBarBaz = "foo-bar-baz";
-
-type DoNothing = KebabCase<"do-nothing">;
-const doNothing: DoNothing = "do-nothing";
+type case1 = IsUnion<string>; // false
+type case2 = IsUnion<string | number>; // true
+type case3 = IsUnion<[string | number]>; // false
