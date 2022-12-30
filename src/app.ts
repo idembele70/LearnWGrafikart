45dfe4a7e17ebc36719b0ca34dd2c21d66b5b393
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-12-30 16:12:20 +0100

    [Type Challenge] RequiredByKeys Done

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,14 @@
 // type challenges
 
 interface User {
-  name: string;
-  age: number;
-  address: string;
+  name?: string
+  age?: number
+  address?: string
 }
-type RemoveUndefined<T, K extends keyof T = keyof T> = T[K] extends infer R | undefined ? R : T[K]
-type PartialByKeys<O, K extends string> = {
-  [Key in keyof O as Key extends K ? Key : never]?: RemoveUndefined<O, Key>;
-} & {
-    [Key in keyof O as Key extends K ? never : Key]: O[Key];
-  };
-
-
-type UserPartialName = PartialByKeys<User, "name">; // { name?:string; age:number; address:string }
-
-type UserPartialNamee = PartialByKeys<User, "name" | "unknown">; // { name?:string; age:number; address:string }
+type Merge<O> = {
+  [Key in keyof O]: O[Key]
+}
+type RequiredByKeys<O, K extends keyof O> = Merge<{
+  [Key in keyof O as Key extends K ? Key : never]-?: O[Key]
+} & Omit<O, K>>
+type UserRequiredName = RequiredByKeys<User, 'name'> // { name: string; age?: number; address?: string }
