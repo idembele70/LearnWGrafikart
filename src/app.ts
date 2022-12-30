b5d4e8f6a2540f7b45197f35e7f0cf0fa3c7e5a2
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-12-30 16:16:54 +0100

    [Type Challenge] Mutable Done

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
 // type challenges
 
-interface User {
-  name?: string
-  age?: number
-  address?: string
+interface Todo {
+  readonly title: string
+  readonly description: string
+  readonly completed: boolean
 }
-type Merge<O> = {
-  [Key in keyof O]: O[Key]
+
+type Mutable<O extends object> = {
+  -readonly [Key in keyof O]: O[Key]
 }
-type RequiredByKeys<O, K extends keyof O> = Merge<{
-  [Key in keyof O as Key extends K ? Key : never]-?: O[Key]
-} & Omit<O, K>>
-type UserRequiredName = RequiredByKeys<User, 'name'> // { name: string; age?: number; address?: string }
+
+type MutableTodo = Mutable<Todo> // { title: string; description: string; completed: boolean; }
