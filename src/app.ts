4a381b00e21ad05036750e65741c1a8c32cda2a9
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-12-30 16:08:11 +0100

    [Type Challenge] PartialByKeys Done

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,14 @@ interface User {
   age: number;
   address: string;
 }
-type RemoveUndefined<T, K extends keyof T> = T[K] extends infer R | undefined ? R : T[K]
+type RemoveUndefined<T, K extends keyof T = keyof T> = T[K] extends infer R | undefined ? R : T[K]
 type PartialByKeys<O, K extends string> = {
   [Key in keyof O as Key extends K ? Key : never]?: RemoveUndefined<O, Key>;
 } & {
     [Key in keyof O as Key extends K ? never : Key]: O[Key];
   };
 
+
 type UserPartialName = PartialByKeys<User, "name">; // { name?:string; age:number; address:string }
+
+type UserPartialNamee = PartialByKeys<User, "name" | "unknown">; // { name?:string; age:number; address:string }
