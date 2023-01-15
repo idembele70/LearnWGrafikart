9c8c3fc41f7a474bd12c28191cbbc6b7722ba07f
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2023-01-15 01:09:05 +0100

    [Type Challenge] ObjectEntries

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 // type challenges
 
-type OmitByType<O, T> = {
-  [Key in keyof O as O[Key] extends T ? never : Key]: O[Key]
+interface Model {
+  name: string;
+  age: number;
+  locations: string[] | null;
 }
 
-type OmitBoolean = OmitByType<{
-  name: string
-  count: number
-  isReadonly: boolean
-  isEnable: boolean
-}, boolean> // { name: string; count: number }
+type RemoveUndefinedIfNeed<T, K extends keyof T> = T[K] extends infer R | undefined ? R : T
+
+type ObjectEntries<O> = {
+  [Key in keyof O]-?: [Key, RemoveUndefinedIfNeed<O, Key>]
+}[keyof O]
+
+type modelEntries = ObjectEntries<Model> // ['name', string] | ['age', number] | ['locations', string[] | null];
+type A = ObjectEntries<Partial<Model>>
