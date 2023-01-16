c9f85c89e498150d5cb77bf3b629921c9e591a94
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2023-01-16 01:32:16 +0100

    [Type Challenge] Reverse

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,7 @@
 // type challenges
 
-type TupleToNestedObject<A extends any[], T> =
-  A extends [infer First, ...infer Rest] ?
-  {
-    [key in `${First & string}`]: Rest["length"] extends 0 ? T : TupleToNestedObject<Rest, T>
-  } : T
+type Reverse<A extends any[]> = A extends [infer First, ...infer Rest] ?
+  [...Reverse<Rest>, First] : A
 
-type a = TupleToNestedObject<['a'], string> // {a: string}
-type b = TupleToNestedObject<['a', 'b'], number> // {a: {b: number}}
-type c = TupleToNestedObject<[], boolean> // boolean. if the tuple is empty, just return the U type
+type a = Reverse<['a', 'b']> // ['b', 'a']
+type b = Reverse<['a', 'b', 'c']> // ['c', 'b', 'a']
