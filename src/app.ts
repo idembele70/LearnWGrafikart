a5363eaef591b5b572c0f8089b5af38d1cf4b8cc
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2023-01-16 01:30:31 +0100

    [Type Challenge] Tuple to nested Object

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,11 @@
 // type challenges
 
-type Result = Shift<[3, 2, 1]> // [2, 1]
-type Shift<A extends any[]> = A extends [any, ...infer Rest] ? Rest : A
+type TupleToNestedObject<A extends any[], T> =
+  A extends [infer First, ...infer Rest] ?
+  {
+    [key in `${First & string}`]: Rest["length"] extends 0 ? T : TupleToNestedObject<Rest, T>
+  } : T
+
+type a = TupleToNestedObject<['a'], string> // {a: string}
+type b = TupleToNestedObject<['a', 'b'], number> // {a: {b: number}}
+type c = TupleToNestedObject<[], boolean> // boolean. if the tuple is empty, just return the U type
