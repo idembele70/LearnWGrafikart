6a20ef0881cfa018004c3ee1bdf6b23c91722787
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2023-01-16 01:35:30 +0100

    [Type Challenge] Flip Arguments

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 // type challenges
 
+type Flipped = FlipArguments<(arg0: string, arg1: number, arg2: boolean) => void>
+// (arg0: boolean, arg1: number, arg2: string) => void
 type Reverse<A extends any[]> = A extends [infer First, ...infer Rest] ?
   [...Reverse<Rest>, First] : A
-
-type a = Reverse<['a', 'b']> // ['b', 'a']
-type b = Reverse<['a', 'b', 'c']> // ['c', 'b', 'a']
+type FlipArguments<F extends Function> =
+  F extends (...args: infer Args) => infer R ?
+  (...arg: Reverse<Args>) => R : never
