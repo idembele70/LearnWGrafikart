8b6975bfdca5f5d1dfed13c49457801c966e1fb3
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2023-01-17 23:56:09 +0100

    [Type Challenge] FlattenDepth Trying

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 // type challenges
 
-type Flipped = FlipArguments<(arg0: string, arg1: number, arg2: boolean) => void>
-// (arg0: boolean, arg1: number, arg2: string) => void
-type Reverse<A extends any[]> = A extends [infer First, ...infer Rest] ?
-  [...Reverse<Rest>, First] : A
-type FlipArguments<F extends Function> =
-  F extends (...args: infer Args) => infer R ?
-  (...arg: Reverse<Args>) => R : never
+type MinusOne<N extends number, A extends any[] = []> =
+  A["length"] extends N ?
+  A extends [any, ...infer Rest] ? Rest & number : 0 : MinusOne<N, [...A, N]>
+
+type FlattenDepth<A extends any[], Count extends number = 1> =
+  Count extends 1 ? Flatten<A> :
+  Flatten<A> extends A ? A :
+  FlattenDepth<Flatten<A>, MinusOne<Count>>
+type Flatten<A extends any[]> =
+  A extends [infer First, ...infer Rest] ?
+  First extends any[] ? [...First, ...Flatten<Rest>] :
+  [First, ...Flatten<Rest>] : A
+
+type C = Flatten<[1, 2, [3, 4], [[[5]]]]>
+type a = FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2> // [1, 2, 3, 4, [5]]. flattern 2 times
+type b = FlattenDepth<[1, 2, [3, 4], [[[5]]]]> // [1, 2, 3, 4, [[5]]]. Depth defaults to be 1
