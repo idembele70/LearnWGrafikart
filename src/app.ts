4794b57d4ec28c15df16e7e435090a3c5a0760c3
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2023-01-18 23:53:20 +0100

    [Type Challenge] FlattenDepth

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
 // type challenges
 
-type MinusOne<N extends number, A extends any[] = []> =
+/* type MinusOne<N extends number, A extends any[] = []> =
   A["length"] extends N ?
-  A extends [any, ...infer Rest] ? Rest & number : 0 : MinusOne<N, [...A, N]>
+  A extends [any, ...infer Rest] ? Rest["length"] : 0 : MinusOne<N, [...A, N]> */
 
-type FlattenDepth<A extends any[], Count extends number = 1> =
-  Count extends 1 ? Flatten<A> :
-  Flatten<A> extends A ? A :
-  FlattenDepth<Flatten<A>, MinusOne<Count>>
+type FlattenDepth<A extends any[], Count extends number = 1, idx extends any[] = []> =
+  A extends Flatten<A> ? A :
+  Count extends idx["length"] ? A :
+  FlattenDepth<Flatten<A>, Count, [0, ...idx]>
 type Flatten<A extends any[]> =
   A extends [infer First, ...infer Rest] ?
   First extends any[] ? [...First, ...Flatten<Rest>] :
   [First, ...Flatten<Rest>] : A
 
-type C = Flatten<[1, 2, [3, 4], [[[5]]]]>
+
+
 type a = FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2> // [1, 2, 3, 4, [5]]. flattern 2 times
 type b = FlattenDepth<[1, 2, [3, 4], [[[5]]]]> // [1, 2, 3, 4, [[5]]]. Depth defaults to be 1
+type c = FlattenDepth<[1, [2, [3, [4, [5]]]]], 19260817> // [1, 2, 3, 4, 5]
+type d = FlattenDepth<[1, [2, [3, [4, [5]]]]], 4> // [1, 2, 3, 4, 5]
