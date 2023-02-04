5e004f03419cd876a5f8c3a18a9e8c62fd679427
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2023-02-04 23:43:36 +0100

    [Type Challenge] InorderTraversal

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,27 @@
 // type challenges
 
-type BEM<B extends string, E extends string[], M extends string[]> =
-  `${B}${[E[number]] extends [never] ? "" : `___${E[number]}`}${[M[number]] extends [never] ? "" : `--${M[number]}`}`
-type a = BEM<'btn', ['price'], []> //  'btn__price'
-type b = BEM<'btn', ['price'], ['warning', 'success']> //  'btn__price--warning' | 'btn__price--success' 
-type c = BEM<'btn', [], ['small', 'medium', 'large']> //  'btn--small' | 'btn--medium' | 'btn--large' 
+const tree1 = {
+  val: 1,
+  left: null,
+  right: {
+    val: 2,
+    left: {
+      val: 3,
+      left: null,
+      right: null,
+    },
+    right: null,
+  },
+} as const
+interface TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+}
+type Traversal<T, U extends keyof T> =
+  T[U] extends TreeNode ? InorderTraversal<T[U]> : []
+type InorderTraversal<T extends TreeNode | null> =
+  T extends TreeNode ?
+  [...Traversal<T, "left">, T["val"], ...Traversal<T, "right">] : T
+
+type A = InorderTraversal<typeof tree1> // [1, 3, 2]
